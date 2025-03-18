49e8b54d4bc9104f5f42cdaf1f860ddde6861146
Sergey <skauzerlfc@gmail.com>
2025-03-18 14:41:32 +0300

    Исправление ссылки

--- src/api/proxy.js
+++ src/api/proxy.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const backendUrl = import.meta.env.VITE_URL;
-  const targetUrl = backendUrl + req.url.replace("/api/proxy", "");
+  const targetUrl = backendUrl + req.url.replace("/api/proxy/", "");
   console.log(targetUrl);
   try {
     const response = await fetch(targetUrl, {
